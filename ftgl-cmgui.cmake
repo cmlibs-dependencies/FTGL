9e61f3432411a4143c35d40c622dc07a94f199d8
Alan Wu <alan.wu@auckland.ac.nz>
2012-10-03 17:11:26 +1300

    Added cmake files

---
+++ ftgl-cmgui.cmake
@@ -0,0 +1,32 @@
+SET( FTGL_SRCS
+	src/FTBuffer.cpp 
+	src/FTCharmap.cpp
+	src/FTContour.cpp
+	src/FTFace.cpp
+	src/FTGlyphContainer.cpp
+	src/FTLibrary.cpp
+	src/FTPoint.cpp
+	src/FTSize.cpp
+	src/FTVectoriser.cpp
+	src/FTFont/FTBitmapFont.cpp
+	src/FTFont/FTExtrudeFont.cpp
+	src/FTFont/FTFontGlue.cpp
+	src/FTFont/FTPixmapFont.cpp
+	src/FTFont/FTTextureFont.cpp
+	src/FTFont/FTBufferFont.cpp
+	src/FTFont/FTFont.cpp
+	src/FTFont/FTOutlineFont.cpp
+	src/FTFont/FTPolygonFont.cpp
+	src/FTGlyph/FTBitmapGlyph.cpp
+	src/FTGlyph/FTExtrudeGlyph.cpp
+	src/FTGlyph/FTGlyphGlue.cpp
+	src/FTGlyph/FTPixmapGlyph.cpp
+	src/FTGlyph/FTTextureGlyph.cpp
+	src/FTGlyph/FTBufferGlyph.cpp
+	src/FTGlyph/FTGlyph.cpp
+	src/FTGlyph/FTOutlineGlyph.cpp
+	src/FTGlyph/FTPolygonGlyph.cpp
+	src/FTLayout/FTLayout.cpp
+	src/FTLayout/FTLayoutGlue.cpp
+	src/FTLayout/FTSimpleLayout.cpp )
+
---
+++ ftgl_config.h.cmake
@@ -0,0 +1,46 @@
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to the path to a TrueType font */
+#cmakedefine FONT_FILE @FONT_FILE@
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#cmakedefine01 HAVE_INTTYPES_H
+
+/* Define to 1 if you have the <memory.h> header file. */
+#cmakedefine01 HAVE_MEMORY_H
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#cmakedefine01 HAVE_STDINT_H
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#cmakedefine01 HAVE_STDLIB_H
+
+/* Define to 1 if you have the <strings.h> header file. */
+#cmakedefine01 HAVE_STRINGS_H
+
+/* Define to 1 if you have the <string.h> header file. */
+#cmakedefine01 HAVE_STRING_H
+
+/* Define to 1 if you have the `strndup' function. */
+#cmakedefine01 HAVE_STRNDUP
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#cmakedefine01 HAVE_SYS_STAT_H
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#cmakedefine01 HAVE_SYS_TYPES_H
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#cmakedefine01 HAVE_UNISTD_H
+
+/* Define to 1 if you have the `wcsdup' function. */
+#cmakedefine01 HAVE_WCSDUP
+
+/* Define to 1 if your C compiler doesn't accept -c and -o together. */
+#cmakedefine01 NO_MINUS_C_MINUS_O
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Define to 1 if the X Window System is missing or not being used. */
+#cmakedefine01 X_DISPLAY_MISSING

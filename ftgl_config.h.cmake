571ad2ac928d0b1837b948fecba328bfdb4e3a71
Alan Wu <alan.wu@auckland.ac.nz>
2012-10-10 10:41:18 +1300

    Minor changes on ftgl_config.h.cmake

--- ftgl_config.h.cmake
+++ ftgl_config.h.cmake
@@ -1,37 +1,42 @@
 /* config.h.in.  Generated from configure.ac by autoheader.  */
+#ifndef FTGL_CONFIG_H_
+#define FTGL_CONFIG_H_
 
 /* Define to 1 if you have the <inttypes.h> header file. */
-#cmakedefine01 HAVE_INTTYPES_H
+#cmakedefine HAVE_INTTYPES_H
 
 /* Define to 1 if you have the <memory.h> header file. */
-#cmakedefine01 HAVE_MEMORY_H
+#cmakedefine HAVE_MEMORY_H
 
 /* Define to 1 if you have the <stdint.h> header file. */
-#cmakedefine01 HAVE_STDINT_H
+#cmakedefine HAVE_STDINT_H
 
 /* Define to 1 if you have the <stdlib.h> header file. */
-#cmakedefine01 HAVE_STDLIB_H
+#cmakedefine HAVE_STDLIB_H
 
 /* Define to 1 if you have the <strings.h> header file. */
-#cmakedefine01 HAVE_STRINGS_H
+#cmakedefine HAVE_STRINGS_H
 
 /* Define to 1 if you have the <string.h> header file. */
-#cmakedefine01 HAVE_STRING_H
+#cmakedefine HAVE_STRING_H
 
 /* Define to 1 if you have the `strndup' function. */
-#cmakedefine01 HAVE_STRNDUP
+#cmakedefine HAVE_STRNDUP
 
 /* Define to 1 if you have the <sys/stat.h> header file. */
-#cmakedefine01 HAVE_SYS_STAT_H
+#cmakedefine HAVE_SYS_STAT_H
 
 /* Define to 1 if you have the <sys/types.h> header file. */
-#cmakedefine01 HAVE_SYS_TYPES_H
+#cmakedefine HAVE_SYS_TYPES_H
 
 /* Define to 1 if you have the `wcsdup' function. */
-#cmakedefine01 HAVE_WCSDUP
+#cmakedefine HAVE_WCSDUP
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Define to 1 if the X Window System is missing or not being used. */
 #cmakedefine01 X_DISPLAY_MISSING
+
+#endif
+
